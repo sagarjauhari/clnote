77e2b29d3dccce55ef182e570a23f4fa9f56a185
Sagar Jauhari <sagarjauhari@gmail.com>
2015-08-27 00:08:49 -0400

    Adding angular

--- src/clnote/views/layout.clj
+++ src/clnote/views/layout.clj
@@ -32,3 +32,36 @@
             [:li
              [:a {:href "/angular"} "Angular"]]]]]]
            [:div {:class "container"} content]]))
+
+(defn angular-application [title & content]
+  (html5 {:lang "en" :ng-app "clnote"}
+    [:head
+      [:meta
+        {:http-equiv "content-type", :content "text/html;charset=utf-8"}]
+      [:title title]
+      (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css")
+      (include-css "//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css")
+      (include-css "vendor/css/dragula.min.css")
+      (include-css "css/screen.css")
+
+      (include-js "//code.jquery.com/jquery-2.1.1.min.js")
+      (include-js "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js")
+      (include-js "//cdnjs.cloudflare.com/ajax/libs/dragula/3.0.3/dragula.min.js")
+      (include-js "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js")
+      (include-js "js/drag-drop.js")
+      (include-js "js/task.js")]
+      (include-js "vendor/js/bootstrap-notify.min.js")
+      (include-js "//ajax.googleapis.com/ajax/libs/angularjs/1.3.18/angular.min.js")
+
+      [:body
+        [:div {:class "navbar navbar-inverse navbar-fixed-top"}  
+         [:div {:class "container"}  
+          [:div {:class "navbar-header"}  
+           [:a {:class "navbar-brand", :href "/tasks"} "CLNote"] ]  
+          [:div {:class "navbar-collapse collapse "}  
+           [:ul {:class "nav navbar-nav"}  
+            [:li
+             [:a {:href "/tasks"} "Tasks"]]
+            [:li
+             [:a {:href "/angular"} "Angular"]]]]]]
+           [:div {:class "container"} content]]))
